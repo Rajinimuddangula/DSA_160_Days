480ee77bbde0a317071642679644fb61c2b32ffe
rajini <rajinimuddangula@gmail.com>
2025-05-29 09:35:41 +0530

    DAY18

---
+++ SearchPattern.java
@@ -0,0 +1,79 @@
+/*Search Pattern (KMP-Algorithm)
+Difficulty: HardAccuracy: 45.04%Submissions: 115K+Points: 8
+Given two strings, one is a text string txt and the other is a pattern string pat. The task is to print the indexes of all the occurrences of the pattern string in the text string. Use 0-based indexing while returning the indices. 
+Note: Return an empty list in case of no occurrences of pattern.
+
+Examples :
+
+Input: txt = "abcab", pat = "ab"
+Output: [0, 3]
+Explanation: The string "ab" occurs twice in txt, one starts at index 0 and the other at index 3. 
+Input: txt = "abesdu", pat = "edu"
+Output: []
+Explanation: There's no substring "edu" present in txt.
+Input: txt = "aabaacaadaabaaba", pat = "aaba"
+Output: [0, 9, 12]
+Explanation:
+
+Constraints:
+1 ≤ txt.size() ≤ 106
+1 ≤ pat.size() < txt.size()
+Both the strings consist of lowercase English alphabets.*/
+import java.util.*;
+class SearchPattern{
+    static void constructLps(String pat, int[] lps){
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+        while (i < pat.length()){
+            if (pat.charAt(i) == pat.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if (len != 0){
+                    len = lps[len - 1];
+                } 
+                else{
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    static ArrayList<Integer> search(String pat, String txt){
+        int n = txt.length();
+        int m = pat.length();
+        int[] lps = new int[m];
+        ArrayList<Integer> res = new ArrayList<>();
+        constructLps(pat, lps);
+        int i = 0;
+        int j = 0;
+        while (i < n){
+            if (txt.charAt(i) == pat.charAt(j)){
+                i++;
+                j++;
+                if (j == m){
+                    res.add(i - j);
+                    j = lps[j - 1];
+                }
+            }
+            else{
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+        return res; 
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String txt =sc.next(); 
+        String pat =sc.next(); 
+        ArrayList<Integer> res = search(pat, txt);
+        for (int i = 0; i < res.size(); i++) 
+            System.out.print(res.get(i) + " ");
+    }
+}
